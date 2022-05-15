470eb3d689e7280e733324d0952aef41704aedff
Owen McClure <omcclu@gmail.com>
2022-05-14 20:32:57 -0400

    getting close to flaskpart

--- wikireader.py
+++ wikireader.py
@@ -69,7 +69,7 @@ class wikiClues:
         og_paragraph = paragraph
         print(og_paragraph)
         paragraph = re.sub(r'\(([^\)]+)\)', '', paragraph, flags=re.M) # remove ()
-        paragraph = self.clean_clue(paragraph, pg_name) # no keywords
+        paragraph = self.scrub_keywords(paragraph, pg_name) # no keywords
         # clue 1 (no proper nouns or keywords)
         clue1 = self.scrub_prop_noun(paragraph)
         clue_list.append(clue1)
@@ -82,31 +82,65 @@ class wikiClues:
         return clue_list
 
         
-    def get_clues_api(self, article):        
-        text = self.parse(article[0])
-        #print(text)
-        header_re = re.compile(u'\ufffd.*$', flags=re.M|re.UNICODE)
-        headers = []
-        for match in header_re.findall(text):
-            text = text.replace(match, '')
-            match = match.replace('\ufffd', '')
-            match = (' ' * int(int(match[0]) - 2)) + match[1:]
-            headers.append(match)        
+    def get_clues_api(self, search_term):
+        useless_headers = ['See also', 'Notes', 'References', 'Further reading', 'External links']
+        text = self.parse_by_title(search_term)
+        #print(title)
+        headers, content  = self.format_sections(text)
+        
+        content = ''.join(content)
+        
         
         nlp = spacy.load('en_core_web_sm')
-        doc = nlp(text)
+        doc = nlp(content)        
         sentences = [sent for sent in doc.sents]
-        print(len(sentences))
-        #for p in sentences:
-        #   print(p, end='\n==\n')
+        
+        if (len(sentences) < 20):
+            print('article too small')
+            return
+        sen_clues = []
+        sen_clues.append(''.join(sentences[:5]))
+        sen_clues.append(''.join(sentences[5:10]))
+        sen_clues.append(''.join(sentences[10:15]))
+        sen_clues.append(''.join(sentences[15:20]))
+        
+        for clue in sen_clues:
+            clue = self.scrub_keywords(clue, self.title)
+            clue = self.scrub_prop_noun(clue)
+            
+        return sen_clues
 
 
+    def format_sections(self, text):
+        match_pat = re.compile(u'(\ufffd.*$)', flags=re.M|re.UNICODE)
+        junk_headers = ['See also', 'Notes', 'References', 'Further reading', 'External links']
+        headers = []
+        content = []
+        sep_text = match_pat.split(text)
+        content.append(sep_text[0])
+        for i in range(1, len(sep_text), 2):
+            heading = sep_text[i].replace('\ufffd', '')
+            if (heading[1:] in junk_headers):
+                pass
+            elif (int(heading[0]) <= 2):
+                headers.append(self.scrub_keywords(heading[1:], self.title))
+                content.append(self.scrub_parens(sep_text[i+1]))
+            else:
+                content.append(self.scrub_parens(sep_text[i+1]))
+                
+        return headers, content
 
-    def clean_clue(self, clue, pg_name):
-        keywords = pg_name.split(' ')
+
+    def scrub_keywords(self, text, keyword):
+        keywords = keyword.split(' ')
         for word in keywords:
-            clue = re.sub(word, '***', clue, flags=re.M|re.I)
-        return clue
+            text = text.replace(word, '***')
+        return text
+    
+    def scrub_parens(self, text):
+        text = re.sub(r'\(([^\)]+)\)', '', text, flags=re.M) # remove ()
+        text = re.sub(r'\[(.*?)\]', '', text, flags=re.M) # remove []
+        return text
     
     def scrub_prop_noun(self, clue):
         proper_noun = re.compile(r'\b([A-Z][a-z]+)\b', flags=re.M)
@@ -117,6 +151,7 @@ class wikiClues:
         return clue
     
     def censor_prop_noun(self, clue):
+        # must call scrub_proper_noun first
         for noun in self.noun_set:
             for match in re.finditer(noun, clue):
                 strt = match.start()
@@ -127,28 +162,39 @@ class wikiClues:
                 clue = ''.join(clue_char)
         return clue
     
-    def parse(self, title):
+    def parse_by_title(self, title):
         API_URL = "https://en.wikipedia.org/w/api.php"
-        params = {
+        headers = {"User-Agent": "Wikipedle/1.0"}
+        params_query = {
+        	"action": "query",
+        	"format": "json",
+        	"list": "search",
+        	"srsearch": title,
+            "srenablerewrites": 1
+        }
+        req = requests.get(API_URL, headers=headers, params=params_query)
+        res = req.json()
+        page_id = res["query"]["search"][0]['pageid']
+        
+        params_extract = {
         	"action": "query",
         	"format": "json",
         	"prop": "extracts",
-        	"titles": title,
+        	"pageids": page_id,
         	"explaintext": 1,
         	"exsectionformat": "raw"
-        }
-        headers = {"User-Agent": "Wikipedle/1.0"}
-        req = requests.get(API_URL, headers=headers, params=params)
+        }        
+        req = requests.get(API_URL, headers=headers, params=params_extract)
         res = req.json()
-        res = res["query"]["pages"]
-        text = res[next(iter(res))]['extract']
+        text = res["query"]["pages"][str(page_id)]['extract']
+        self.title = res["query"]["pages"][str(page_id)]['title']
         return text
                   
         
         
 def test_non_api(testReader, art):
     i = random.randint(0, len(art))
-    i = 152
+    #i = 152
     print(i)
     ans = art[i][0]
     clue_list = testReader.get_clues(art[i][0], art[i][1])
@@ -157,11 +203,14 @@ def test_non_api(testReader, art):
 
 
 def test_api(testReader, articles):
+    i = random.randint(0, len(art))
     i = 152
-    h = testReader.get_clues_api(art[152])
-    print(h)
+    print(i)
+    #h = testReader.get_clues_api(art[i][0])
+    h = testReader.get_clues_api('Expanse novel')
     #for hs in h:
     #    print(hs)
+    return h, art[i][0]
 
 
 if __name__ == '__main__':
@@ -170,6 +219,6 @@ if __name__ == '__main__':
     testReader = wikiClues()
     art = testReader.get_pop_articles()
     #test_non_api(testReader, art)
-    test_api(testReader, art)
+    clue, ans = test_api(testReader, art)
     
     
